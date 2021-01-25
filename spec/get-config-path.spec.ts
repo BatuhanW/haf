6eedb2835d6b214c59c34cf77c205613f2e3ceb5
Batuhan Wilhelm <batuhanwilhelm@gmail.com>
2021-01-26 00:29:27 +0300

    Fix windows tests, conditionally run tests per env

--- spec/get-config-path.spec.ts
+++ spec/get-config-path.spec.ts
@@ -2,13 +2,21 @@ import os from 'os';
 
 import { getConfigPath } from '../src/get-config-path';
 
+const isWindows = os.platform() === 'win32';
+
+const describeIfWindows = isWindows ? describe : describe.skip;
+
+const describeIfUnix = isWindows ? describe.skip : describe;
+
+const splitKey = isWindows ? '\\' : '/';
+
 describe('get-config-path', () => {
   describe('extension', () => {
     describe('when absent', () => {
       it('should skip extension', () => {
         const path = getConfigPath('pop');
 
-        const index = path.split('/').lastIndexOf('pop');
+        const index = path.split(splitKey).lastIndexOf('pop');
 
         expect(index).toBeGreaterThanOrEqual(0);
       });
@@ -18,7 +26,7 @@ describe('get-config-path', () => {
       it('should append extension', () => {
         const path = getConfigPath('pop', 'dog');
 
-        const index = path.split('/').lastIndexOf('pop.dog');
+        const index = path.split(splitKey).lastIndexOf('pop.dog');
 
         expect(index).toBeGreaterThanOrEqual(0);
       });
@@ -26,76 +34,72 @@ describe('get-config-path', () => {
   });
 
   describe('platforms', () => {
-    describe('when CONFIG_DIR is present', () => {
-      beforeEach(() => {
-        process.env.CONFIG_DIR = '/home/config_dir';
+    describeIfUnix('unix', () => {
+      describe('when CONFIG_DIR is present', () => {
+        beforeEach(() => {
+          process.env.CONFIG_DIR = '/home/config_dir';
+        });
+
+        afterEach(() => {
+          delete process.env.CONFIG_DIR;
+        });
+
+        it('should respect CONFIG_DIR', () => {
+          const path = getConfigPath('pop');
+
+          expect(path).toEqual('/home/config_dir/pop');
+        });
       });
 
-      afterEach(() => {
-        delete process.env.CONFIG_DIR;
+      describe('when XDG_CONFIG_HOME is present', () => {
+        beforeEach(() => {
+          process.env.XDG_CONFIG_HOME = '/home/xdg_config_home';
+        });
+
+        afterEach(() => {
+          delete process.env.XDG_CONFIG_HOME;
+        });
+
+        it('should respect XDG_CONFIG_HOME', () => {
+          const path = getConfigPath('pop');
+
+          expect(path).toEqual('/home/xdg_config_home/pop');
+        });
       });
 
-      it('should respect CONFIG_DIR', () => {
-        const path = getConfigPath('pop');
+      describe('when fallback', () => {
+        const spy = jest.spyOn(os, 'homedir');
 
-        expect(path).toEqual('/home/config_dir/pop');
+        beforeEach(() => {
+          spy.mockReturnValue('/Users/anda');
+        });
+
+        afterEach(() => {
+          spy.mockRestore();
+        });
+
+        it('should put under ~/.config', () => {
+          const path = getConfigPath('pop');
+
+          expect(path).toEqual('/Users/anda/.config/pop');
+        });
       });
     });
 
-    describe('when XDG_CONFIG_HOME is present', () => {
+    describeIfWindows('when windows 😞', () => {
       beforeEach(() => {
-        process.env.XDG_CONFIG_HOME = '/home/xdg_config_home';
-      });
-
-      afterEach(() => {
-        delete process.env.XDG_CONFIG_HOME;
-      });
-
-      it('should respect XDG_CONFIG_HOME', () => {
-        const path = getConfigPath('pop');
-
-        expect(path).toEqual('/home/xdg_config_home/pop');
-      });
-    });
-
-    describe('when windows 😞', () => {
-      const spy = jest.spyOn(os, 'platform');
-
-      beforeEach(() => {
-        process.env.LOCALAPPDATA = 'C:/Users/Pop/ApplicationData';
-
-        spy.mockReturnValue('win32');
+        process.env.LOCALAPPDATA = 'C:\\Users\\Pop\\ApplicationData';
       });
 
       afterEach(() => {
         delete process.env.LOCALAPPDATA;
-
-        spy.mockRestore();
       });
 
       it('should deal with WINDOWS', () => {
         const path = getConfigPath('pop');
 
-        expect(path).toEqual('C:/Users/Pop/ApplicationData/pop');
+        expect(path).toEqual('C:\\Users\\Pop\\ApplicationData\\pop');
       });
     });
-
-    describe('when fallback', () => {
-      const spy = jest.spyOn(os, 'homedir')
-
-      beforeEach(() => {
-        spy.mockReturnValue('/Users/anda')
-      })
-
-      afterEach(() => {
-        spy.mockRestore()
-      })
-
-      it('should put under ~/.config', () => {
-        const path = getConfigPath('pop')
-
-        expect(path).toEqual('/Users/anda/.config/pop')
-      })
-    })
   });
 });
